4a3ac6487fd8196c3a8645ded4246580ecd3df39
Oscar Sanz <osanzce@gmail.com>
2023-01-04 21:13:34 +0100

    test: add allergens tests for updating actions

---
+++ src/modules/allergens/tests/updateAllergen.test.ts
@@ -0,0 +1,136 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import request from 'supertest';
+import { createApp } from '../../../app';
+import * as AllergenService from '../allergens.service';
+
+const app = createApp();
+
+const allergenPayload = {
+  _id: '639eea5a049fc933bddebab2',
+  name: 'test',
+  icon: 'test allergen icon',
+};
+
+const nameUpdateInput = 'updated';
+
+const iconUpdateInput = 'updated';
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  jest.resetAllMocks();
+});
+
+const updateAllergenServiceMock = jest.spyOn(AllergenService, 'updateAllergen');
+
+describe('HAPPY PATH', () => {
+  describe('should return 200 and update allergen', () => {
+    it('should return updated name', async () => {
+      updateAllergenServiceMock
+        // @ts-ignore
+        .mockReturnValueOnce({ ...allergenPayload, name: nameUpdateInput });
+
+      const { statusCode, body } = await request(app)
+        .patch(`/api/v1/allergens/${allergenPayload._id}`)
+        .send({ name: nameUpdateInput });
+
+      expect(statusCode).toBe(200);
+      expect(body.data).toMatchObject({ ...allergenPayload, name: nameUpdateInput });
+      expect(body.data.name).toEqual(nameUpdateInput);
+      expect(updateAllergenServiceMock).toHaveBeenNthCalledWith(1, {
+        allergenId: allergenPayload._id,
+        update: { name: nameUpdateInput },
+      });
+    });
+
+    it('should return updated icon', async () => {
+      updateAllergenServiceMock
+        // @ts-ignore
+        .mockReturnValueOnce({ ...allergenPayload, icon: iconUpdateInput });
+
+      const { statusCode, body } = await request(app)
+        .patch(`/api/v1/allergens/${allergenPayload._id}`)
+        .send({ icon: iconUpdateInput });
+
+      expect(statusCode).toBe(200);
+      expect(body.data).toMatchObject({ ...allergenPayload, icon: iconUpdateInput });
+      expect(body.data.icon).toEqual(iconUpdateInput);
+      expect(updateAllergenServiceMock).toHaveBeenNthCalledWith(1, {
+        allergenId: allergenPayload._id,
+        update: { icon: iconUpdateInput },
+      });
+    });
+
+    it('should return updated name and icon', async () => {
+      updateAllergenServiceMock
+        // @ts-ignore
+        .mockReturnValueOnce({ name: nameUpdateInput, icon: iconUpdateInput });
+
+      const { statusCode, body } = await request(app)
+        .patch(`/api/v1/allergens/${allergenPayload._id}`)
+        .send({ name: nameUpdateInput, icon: iconUpdateInput });
+
+      expect(statusCode).toBe(200);
+      expect(body.data).toMatchObject({ name: nameUpdateInput, icon: iconUpdateInput });
+      expect(body.data.name).toEqual(nameUpdateInput);
+      expect(body.data.icon).toEqual(iconUpdateInput);
+      expect(updateAllergenServiceMock).toHaveBeenNthCalledWith(1, {
+        allergenId: allergenPayload._id,
+        update: { name: nameUpdateInput, icon: iconUpdateInput },
+      });
+    });
+  });
+});
+
+describe('UNHAPPY PATH', () => {
+  describe('wrong id format provided', () => {
+    it('should return 400', async () => {
+      const { statusCode, body } = await request(app).patch(
+        `/api/v1/allergens/ee639a5a049fc933bdde`
+      );
+
+      expect(statusCode).toBe(400);
+      expect(body.error).toBeDefined();
+      expect(body.error).toMatch(/invalid id/i);
+      expect(updateAllergenServiceMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('wrong id provided', () => {
+    it('should return 404', async () => {
+      // @ts-ignore
+      updateAllergenServiceMock.mockReturnValueOnce(null);
+
+      const { statusCode, body } = await request(app)
+        .patch(`/api/v1/allergens/ee639a5a049fc933bddebab3`)
+        .send({ name: nameUpdateInput, icon: iconUpdateInput });
+
+      expect(statusCode).toBe(404);
+      expect(body.error).toBeDefined();
+      expect(body.error).toMatch(/allergen not found to update/i);
+      expect(updateAllergenServiceMock).toHaveBeenNthCalledWith(1, {
+        allergenId: 'ee639a5a049fc933bddebab3',
+        update: { name: nameUpdateInput, icon: iconUpdateInput },
+      });
+    });
+  });
+
+  describe('unexpected error', () => {
+    it('should return 500', async () => {
+      updateAllergenServiceMock.mockRejectedValueOnce('ooops');
+
+      const { statusCode, body } = await request(app)
+        .patch(`/api/v1/allergens/${allergenPayload._id}`)
+        .send({ name: nameUpdateInput, icon: iconUpdateInput });
+
+      expect(statusCode).toBe(500);
+      expect(body.error).toBeDefined();
+      expect(body.error).toMatch(/unexpected internal error/i);
+      expect(updateAllergenServiceMock).toHaveBeenNthCalledWith(1, {
+        allergenId: allergenPayload._id,
+        update: { name: nameUpdateInput, icon: iconUpdateInput },
+      });
+    });
+  });
+});
