3164818999af3fa17fa12376b6e53931489ff230
Oscar Sanz <osanzce@gmail.com>
2023-01-07 14:58:11 +0100

    feat: git add ingredients routes to app

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { allergensRouter } from './modules/allergens/allergens.router';
+import { ingredientsRouter } from './modules/ingredients/ingredients.router';
 
 export const apiRouter = Router();
 
 apiRouter.use('/allergens', allergensRouter);
+apiRouter.use('/ingredients', ingredientsRouter);
