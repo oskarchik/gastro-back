3db9ebc885b8c9840cc426d7d07db43e42872147
Oscar Sanz <osanzce@gmail.com>
2023-01-12 21:30:30 +0100

    feat: add schema validation to ingredients module

---
+++ src/modules/ingredients/ingredients.schema.ts
@@ -0,0 +1,104 @@
+import { z } from 'zod';
+
+const categoryNames = [
+  'meat',
+  'fish',
+  'seafood',
+  'vegetable',
+  'legume',
+  'dairy',
+  'fruit',
+  'cereal',
+  'fat',
+  'eggs',
+  'herbs and spices',
+  'sauces',
+  'others',
+] as const;
+const allergenNames = [
+  'gluten',
+  'peanuts',
+  'tree nuts',
+  'celery',
+  'mustard',
+  'eggs',
+  'milk',
+  'sesame',
+  'fish',
+  'crustaceans',
+  'molluscs',
+  'soya',
+  'sulphites',
+  'lupin',
+] as const;
+
+const payload = {
+  body: z.object({
+    name: z.string({
+      required_error: 'Name is required',
+    }),
+    category: z.enum(categoryNames).optional(),
+    hasAllergens: z.boolean({
+      required_error: 'Please define if the ingredient contains allergens',
+    }),
+    allergens: z.array(z.string()).optional(),
+    allergenNames: z.array(z.enum(allergenNames)).optional(),
+  }),
+};
+
+const updatePayload = {
+  body: z.object({
+    name: z.string().optional(),
+    category: z.enum(categoryNames).optional(),
+    hasAllergens: z.boolean().optional(),
+    allergens: z.array(z.string()).optional(),
+    allergenNames: z.array(z.enum(allergenNames)).optional(),
+  }),
+};
+
+const params = {
+  params: z.object({
+    id: z.string({
+      required_error: 'Id is required',
+    }),
+  }),
+};
+
+const query = {
+  query: z.object({
+    name: z.string().optional(),
+    category: z.enum(categoryNames).optional(),
+    hasAllergens: z.string().optional(),
+    allergens: z.array(z.string()).optional(),
+    allergenNames: z.enum(allergenNames).optional(),
+  }),
+};
+
+export const getIngredientSchema = z.object({
+  ...query,
+});
+
+export const getIngredientByIdSchema = z.object({
+  ...params,
+});
+
+export const createIngredientSchema = z.object({
+  ...payload,
+});
+export const deleteIngredientSchema = z.object({
+  ...query,
+});
+
+export const deleteIngredientByIdSchema = z.object({
+  ...params,
+});
+
+export const updateIngredientSchema = z.object({
+  ...updatePayload,
+  ...params,
+});
+
+export type GetIngredientSchema = z.TypeOf<typeof getIngredientSchema>;
+export type CreateIngredientSchema = z.TypeOf<typeof createIngredientSchema>;
+export type DeleteIngredientSchema = z.TypeOf<typeof deleteIngredientSchema>;
+export type UpdateIngredientSchema = z.TypeOf<typeof updateIngredientSchema>;
