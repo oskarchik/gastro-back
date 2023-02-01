15aedca0c3ef93ac28610bb6202cbcf22dc3e408
Oscar Sanz <osanzce@gmail.com>
2023-02-01 09:02:56 +0100

    refactor(app): move helth route from app to routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,21 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { allergensRouter } from './modules/allergens/allergens.router';
 import { ingredientsRouter } from './modules/ingredients/ingredients.router';
 
 export const apiRouter = Router();
 
+apiRouter.get(
+  '/health',
+
+  (_req: Request, res: Response) => {
+    const data = {
+      uptime: process.uptime(),
+      responseTime: process.hrtime(),
+      message: 'ok',
+      date: new Date(),
+    };
+    return res.status(200).send({ data });
+  }
+);
 apiRouter.use('/allergens', allergensRouter);
 apiRouter.use('/ingredients', ingredientsRouter);
