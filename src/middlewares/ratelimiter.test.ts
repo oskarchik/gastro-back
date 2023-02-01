17f5edb2abc2ac4551f699b25d238b166a2ff7d5
Oscar Sanz <osanzce@gmail.com>
2023-02-01 09:00:41 +0100

    test(rate-limiter): add rate limiter tests

---
+++ src/middlewares/ratelimiter.test.ts
@@ -0,0 +1,56 @@
+import { NextFunction, Request, Response } from 'express';
+import { redis } from 'src/utils/redis';
+import { Limiter, rateLimiter } from './rateLimiter';
+
+const IP_ADDRESS = '127.0.0.1';
+const mockRequest = {
+  headers: {
+    'x-forwarded-for': IP_ADDRESS,
+  },
+} as unknown as Request;
+const mockResponse = {
+  status: jest.fn().mockReturnThis(),
+  send: jest.fn(),
+} as unknown as Response;
+const mockNext: NextFunction = jest.fn();
+
+const rateLimiterOptions: Limiter = {
+  windowSize: 10,
+  allowedRequests: 4,
+};
+
+afterEach(() => {
+  jest.clearAllMocks();
+  redis.del(IP_ADDRESS);
+});
+
+const limiter = rateLimiter(rateLimiterOptions);
+
+describe('rateLimiter', () => {
+  it('should respond 200 when requests are under limit', async () => {
+    await limiter(mockRequest, mockResponse, mockNext);
+    expect(mockNext).toHaveBeenCalledTimes(1);
+  });
+
+  it('should respond 429 when requests are over limit', async () => {
+    for (let i = 0; i <= rateLimiterOptions.allowedRequests; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      await limiter(mockRequest, mockResponse, mockNext);
+    }
+
+    expect(mockNext).toHaveBeenCalledTimes(4);
+    expect(mockResponse.status).toHaveBeenNthCalledWith(1, 429);
+    expect(mockResponse.send).toHaveBeenNthCalledWith(1, { error: 'too many requests' });
+  });
+
+  it('should respond 200 after window time has passed', async () => {
+    const shortLimiter = rateLimiter({ ...rateLimiterOptions, windowSize: 0 });
+
+    for (let i = 0; i < 6; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      await shortLimiter(mockRequest, mockResponse, mockNext);
+    }
+
+    expect(mockNext).toHaveBeenCalledTimes(6);
+  });
+});
