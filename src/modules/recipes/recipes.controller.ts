1fca3b9ebf6be56a8419db38744483ef94e56e0e
Oscar Sanz <osanzce@gmail.com>
2023-02-03 18:40:56 +0100

    feat(recipes-module): add makeRecipe to recipes controller

--- src/modules/recipes/recipes.controller.ts
+++ src/modules/recipes/recipes.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import { ApiError } from 'src/error/ApiError';
 import { filterProperties } from 'src/utils/filterProperties';
-import { getRecipes } from './recipes.service';
+import { createRecipe, getRecipes } from './recipes.service';
 
 export const findRecipes = async (req: Request, res: Response, next: NextFunction) => {
   const properties = [
@@ -19,6 +20,40 @@ export const findRecipes = async (req: Request, res: Response, next: NextFunctio
     const foundRecipes = await getRecipes(filteredQuery);
 
     return res.status(200).send({ data: foundRecipes });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const makeRecipe = async (req: Request, res: Response, next: NextFunction) => {
+  const {
+    name,
+    category,
+    subCategory,
+    ingredients,
+    ingredientNames,
+    hasAllergens,
+    allergens,
+    allergenNames,
+  } = req.body;
+
+  if (!name || hasAllergens === undefined) {
+    return next(ApiError.badRequest('name and hasAllergens are required'));
+  }
+
+  try {
+    const savedRecipe = await createRecipe({
+      name,
+      category,
+      subCategory,
+      ingredients,
+      ingredientNames,
+      hasAllergens,
+      allergens,
+      allergenNames,
+    });
+
+    return res.status(200).send({ data: savedRecipe });
   } catch (error) {
     return next(error);
   }
