832e950fd331dc0cccffd00e7ff1ea4ab67f47d7
Oscar Sanz <osanzce@gmail.com>
2023-02-03 18:58:48 +0100

    feat(recipes-module): add post/recipes route

--- src/modules/recipes/recipes.routes.ts
+++ src/modules/recipes/recipes.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { findRecipes } from './recipes.controller';
+import { findRecipes, makeRecipe } from './recipes.controller';
 import { cache } from 'src/middlewares/cache.middleware';
 import { validate } from 'src/middlewares/validationRequest';
-import { getRecipeSchema } from './recipes.schema';
+import { createRecipeSchema, getRecipeSchema } from './recipes.schema';
 
 export const recipesRouter = Router();
 
 recipesRouter.get('/', validate(getRecipeSchema), cache, findRecipes);
+
+recipesRouter.post('/', validate(createRecipeSchema), makeRecipe);
