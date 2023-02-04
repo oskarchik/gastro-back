beb4c56590e7d0dcd675483d84165b8d854063e1
Oscar Sanz <osanzce@gmail.com>
2023-02-04 11:47:02 +0100

    refactor(redis): createRedisKey can be used now in controller and cache middleware

--- src/utils/redisKey.ts
+++ src/utils/redisKey.ts
@@ -1,14 +1,27 @@
 import { Request } from 'express';
+import { FilterQuery } from 'mongoose';
+import { IngredientInput } from 'src/modules/ingredients/ingredients.model';
+import { RecipeInput } from 'src/modules/recipes/recipes.model';
 import { getRoute } from './getRoute';
 
-export const createRedisKey = (object: Partial<Request>) => {
-  const route = getRoute(object.baseUrl);
+export type KeyFromQuery = {
+  queryObject: Partial<Request> | Partial<FilterQuery<IngredientInput | RecipeInput>>;
+  controller?: string;
+};
 
-  if (object.params?.id) {
-    return `${route}_${object.params.id}`;
+export const createRedisKey = ({ queryObject, controller }: KeyFromQuery) => {
+  const route = getRoute(queryObject.baseUrl) || controller;
+
+  if (queryObject.params?.id) {
+    return `${route}_${queryObject.params.id}`;
   }
-  if (object.query && Object.keys(object.query).length > 0) {
-    return `${route}_${Object.keys(object.query)}`;
+  if (queryObject.query && Object.keys(queryObject.query).length > 0) {
+    return `${route}_${JSON.stringify(queryObject.query)}`;
+  }
+  if (controller) {
+    return Object.keys(queryObject).length > 0
+      ? `${controller}_${JSON.stringify(queryObject)}`
+      : controller;
   }
 
   return route;
