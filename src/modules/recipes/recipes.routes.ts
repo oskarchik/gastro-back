06a48eceb3232b8bea7a2ff5c89cbc4f85defacc
Oscar Sanz <osanzce@gmail.com>
2023-02-05 06:37:34 +0100

    feat(recipes-module): add get recipes by id route

--- src/modules/recipes/recipes.routes.ts
+++ src/modules/recipes/recipes.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { findRecipes, makeRecipe } from './recipes.controller';
+import { findRecipeById, findRecipes, makeRecipe } from './recipes.controller';
 import { cache } from 'src/middlewares/cache.middleware';
 import { validate } from 'src/middlewares/validationRequest';
 import { createRecipeSchema, getRecipeSchema } from './recipes.schema';
@@ -7,5 +7,6 @@ import { createRecipeSchema, getRecipeSchema } from './recipes.schema';
 export const recipesRouter = Router();
 
 recipesRouter.get('/', validate(getRecipeSchema), cache, findRecipes);
+recipesRouter.get('/:id', validate(getRecipeSchema), cache, findRecipeById);
 
 recipesRouter.post('/', validate(createRecipeSchema), makeRecipe);
