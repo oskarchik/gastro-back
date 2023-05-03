f01fdca977c83007776c2e8f5e1e4865d2c56e76
Oscar Sanz <osanzce@gmail.com>
2023-05-03 21:12:57 +0200

    refactor(error): use interfaces from types file

--- src/error/BaseError.ts
+++ src/error/BaseError.ts
@@ -1,21 +1,5 @@
-/* eslint-disable no-shadow */
-export enum HttpCode {
-  OK = 200,
-  NO_CONTENT = 204,
-  BAD_REQUEST = 400,
-  UNAUTHORIZED = 401,
-  FORBIDDEN = 403,
-  NOT_FOUND = 404,
-  INTERNAL_SERVER_ERROR = 500,
-}
+import { HttpCode, BaseErrorArgs } from 'src/types/types';
 
-interface BaseErrorArgs {
-  name?: string;
-  httpCode: HttpCode;
-  // description: string;
-  message: string;
-  isOperational: boolean;
-}
 export class BaseError extends Error {
   public httpCode: HttpCode;
 
