3072f1558b75e3b01075a86e111e6baf4b6f2223
Oscar Sanz <osanzce@gmail.com>
2023-07-10 21:08:00 +0200

    refactor(error): add defualt error messages to ApiError class

--- src/error/ApiError.ts
+++ src/error/ApiError.ts
@@ -2,29 +2,66 @@ import { HttpCode } from 'src/types/types';
 import { BaseError } from './BaseError';
 
 export class ApiError extends BaseError {
+  public static DEFAULT_BAD_REQUEST_MESSAGE =
+    'Invalid request: The request contains invalid data or parameters. Please review the request and try again with valid information.';
+
+  public static DEFAULT_UNAUTHORIZED_MESSAGE =
+    'Unauthorized access: You are not authorized to access this resource. Please provide valid credentials to proceed.';
+
+  public static DEFAULT_FORBIDDEN_MESSAGE =
+    'Forbidden: You are not allowed to perform the requested operation. Please ensure you have the necessary privileges.';
+
+  public static DEFAULT_NOT_FOUND_MESSAGE =
+    'Resource not found: The requested resource could not be found. Please verify the URL and try again.';
+
+  public static DEFAULT_INTERNAL_SERVER_ERROR_MESSAGE =
+    'Internal Server Error: An unexpected error occurred on the server. Please try again later or contact the administrator for assistance.';
+
   constructor(httpCode: number, message: string, isOperational: boolean) {
     super({ httpCode, message, isOperational });
     this.httpCode = httpCode;
     this.isOperational = isOperational;
   }
 
-  static badRequest(msg: string) {
-    return new ApiError(HttpCode.BAD_REQUEST, msg, true);
+  static badRequest(msg = '') {
+    return new ApiError(
+      HttpCode.BAD_REQUEST,
+      msg ? `${this.DEFAULT_BAD_REQUEST_MESSAGE} ${msg}` : `${this.DEFAULT_BAD_REQUEST_MESSAGE}`,
+      true
+    );
   }
 
-  static unauthorized(msg: string) {
-    return new ApiError(HttpCode.UNAUTHORIZED, msg, true);
+  static unauthorized(msg = '') {
+    return new ApiError(
+      HttpCode.UNAUTHORIZED,
+      msg ? `${this.DEFAULT_UNAUTHORIZED_MESSAGE} ${msg}` : `${this.DEFAULT_UNAUTHORIZED_MESSAGE}`,
+      true
+    );
   }
 
-  static forbidden(msg: string) {
-    return new ApiError(HttpCode.FORBIDDEN, msg, true);
+  static forbidden(msg = '') {
+    return new ApiError(
+      HttpCode.FORBIDDEN,
+      msg ? `${this.DEFAULT_FORBIDDEN_MESSAGE} ${msg}` : `${this.DEFAULT_FORBIDDEN_MESSAGE}`,
+      true
+    );
   }
 
-  static notFound(msg: string) {
-    return new ApiError(HttpCode.NOT_FOUND, msg, true);
+  static notFound(msg = '') {
+    return new ApiError(
+      HttpCode.NOT_FOUND,
+      msg ? `${this.DEFAULT_NOT_FOUND_MESSAGE} ${msg}` : `${this.DEFAULT_NOT_FOUND_MESSAGE}`,
+      true
+    );
   }
 
-  static internalError(msg: string) {
-    return new ApiError(HttpCode.INTERNAL_SERVER_ERROR, msg, true);
+  static internalError(msg = '') {
+    return new ApiError(
+      HttpCode.INTERNAL_SERVER_ERROR,
+      msg
+        ? `${this.DEFAULT_INTERNAL_SERVER_ERROR_MESSAGE} ${msg}`
+        : `${this.DEFAULT_INTERNAL_SERVER_ERROR_MESSAGE}`,
+      true
+    );
   }
 }
